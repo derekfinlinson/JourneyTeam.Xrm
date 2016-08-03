d21de3467a995583ec98672dc9e3b227fa110b85
Derek C. Finlinson <Derek.Finlinson@journeyteam.com>
2016-08-03 13:28:03 -0600

    Move test to new project to fix dependency issues with Nuget

--- JourneyTeam.Xrm/JourneyTeam.Xrm/Properties/AssemblyInfo.cs
+++ JourneyTeam.Xrm/JourneyTeam.Xrm/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("JourneyTeam.Xrm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Base Classes for CRM Plugins and Workflows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("JourneyTeam")]
 [assembly: AssemblyProduct("JourneyTeam.Xrm")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
