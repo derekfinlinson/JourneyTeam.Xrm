059eaa0fe4c325525a625e3e3b651afac538978e
Derek C. Finlinson <Derek.Finlinson@journeyteam.com>
2016-08-17 16:42:44 -0600

    Revert to CRM 2013 dependencies

--- JourneyTeam.Xrm/xrm-webapi/typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/del/index.d.ts" />
-/// <reference path="globals/gulp-typescript/index.d.ts" />
-/// <reference path="globals/gulp/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/xrm/index.d.ts" />
