1e38057f7e0b86da328411df5b3db5169b0b0e69
Derek Finlinson <derek.finlinson@journeyteam.com>
2018-01-03 14:12:57 -0700

    Fix getting plugin context properties

--- JourneyTeam.Xrm/JourneyTeam.Xrm/Plugin/LocalPluginContext.cs
+++ JourneyTeam.Xrm/JourneyTeam.Xrm/Plugin/LocalPluginContext.cs
@@ -15,13 +15,13 @@ namespace JourneyTeam.Xrm.Plugin
         /// Microsoft Dynamics CRM organization service
         /// </summary>
         public IOrganizationService OrganizationService { get; private set; }
-        
+
         /// <summary>
         /// IPluginExecutionContext contains information that describes the run-time environment in which the plug-in executes, 
         /// information related to the execution pipeline, and entity business information
         /// </summary>
         public IPluginExecutionContext PluginExecutionContext { get; }
-        
+
         /// <summary>
         /// Synchronous registered plug-ins can post the execution context to the Microsoft Azure Service Bus. <br/> 
         /// It is through this notification service that synchronous plug-ins can send brokered messages to the Microsoft Azure Service Bus
@@ -34,19 +34,30 @@ namespace JourneyTeam.Xrm.Plugin
         public RegisteredEvent Event { get; set; }
 
         /// <summary>
-        /// Post Entity Images
+        /// Gets the pre entity images.
         /// </summary>
-        public EntityImageCollection PostImages { get; set; }
+        /// <value>
+        /// The pre entity images.
+        /// </value>
+        public virtual EntityImageCollection PreEntityImages => PluginExecutionContext.PreEntityImages;
 
         /// <summary>
-        /// Pre Entity Images
+        /// Gets the post entity images.
         /// </summary>
-        public EntityImageCollection PreImages { get; set; }
+        /// <value>
+        /// The post entity images.
+        /// </value>
+        public virtual EntityImageCollection PostEntityImages => PluginExecutionContext.PostEntityImages;
 
         /// <summary>
         /// Plugin InputParameters
         /// </summary>
-        public ParameterCollection InputParameters { get; set; }
+        public ParameterCollection InputParameters => PluginExecutionContext.InputParameters;
+
+        /// <summary>
+        /// Plugin OutputParameters
+        /// </summary>
+        public ParameterCollection OutputParameters => PluginExecutionContext.OutputParameters;
 
         /// <summary>
         /// Provides logging run-time trace information for plug-ins
@@ -64,29 +75,24 @@ namespace JourneyTeam.Xrm.Plugin
             {
                 throw new ArgumentNullException(nameof(serviceProvider));
             }
-            
+
             // Obtain the execution context service from the service provider.
-            this.PluginExecutionContext = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-            
-            // Obtain the post and pre images
-            this.PostImages = this.PluginExecutionContext.PostEntityImages;
-            this.PreImages = this.PluginExecutionContext.PreEntityImages;
-            this.InputParameters = this.InputParameters;
+            PluginExecutionContext = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
 
             // Obtain the tracing service from the service provider.
-            this._tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            _tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
 
             // Get the notification service from the service provider.
-            this.NotificationService = (IServiceEndpointNotificationService)serviceProvider.GetService(typeof(IServiceEndpointNotificationService));
+            NotificationService = (IServiceEndpointNotificationService)serviceProvider.GetService(typeof(IServiceEndpointNotificationService));
 
             // Obtain the organization factory service from the service provider.
-            this._factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-            
+            _factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
             // Use the factory to generate the organization service.
-            this.OrganizationService = CreateOrganizationService(this.PluginExecutionContext.UserId);
+            OrganizationService = CreateOrganizationService(PluginExecutionContext.UserId);
 
             // Set Event
-            this.Event = this.PluginExecutionContext.GetEvent(events);
+            Event = PluginExecutionContext.GetEvent(events);
         }
 
         /// <summary>
@@ -97,7 +103,7 @@ namespace JourneyTeam.Xrm.Plugin
         /// <remarks>Useful for impersonation</remarks>
         public IOrganizationService CreateOrganizationService(Guid userId)
         {
-            return this._factory.CreateOrganizationService(userId);
+            return _factory.CreateOrganizationService(userId);
         }
 
         /// <summary>
@@ -106,12 +112,12 @@ namespace JourneyTeam.Xrm.Plugin
         /// <param name="message">Message name to trace.</param>
         public void Trace(string message)
         {
-            if (string.IsNullOrWhiteSpace(message) || this._tracingService == null)
+            if (string.IsNullOrWhiteSpace(message) || _tracingService == null)
             {
                 return;
             }
-            
-            this._tracingService.Trace(message);
+
+            _tracingService.Trace(message);
         }
     }
 }
