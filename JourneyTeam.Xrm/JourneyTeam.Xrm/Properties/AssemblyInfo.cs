2808ca2f5a043860fe66a95d19e118980726efe7
Derek Finlinson <Derek.Finlinson@journeyteam.com>
2018-09-14 11:21:28 -0600

    Switch packages

--- JourneyTeam.Xrm/JourneyTeam.Xrm/Properties/AssemblyInfo.cs
+++ JourneyTeam.Xrm/JourneyTeam.Xrm/Properties/AssemblyInfo.cs
@@ -21,17 +21,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2caf9afc-1bf6-4a38-a3b0-5122b6369a4e")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2016.8.24")]
-[assembly: AssemblyFileVersion("1.2016.8.24")]
-
 [assembly: Microsoft.Xrm.Sdk.Client.ProxyTypesAssembly]
