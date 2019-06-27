d2d36571b5a39a3f83368af64d9e24fea7e3847f
Derek Finlinson <dcfinlinson@gmail.com>
2019-06-27 16:40:56 -0600

    Add IOrganizationService extension methods

---
+++ Extensions/IOrganizationServiceExtensions.cs
@@ -0,0 +1,22 @@
+using Microsoft.Xrm.Sdk;
+
+namespace Xrm
+{
+    public static class IOrganizationServiceExtensions
+    {
+        public static void Delete(this IOrganizationService service, EntityReference reference)
+        {
+            service.Delete(reference.LogicalName, reference.Id);
+        }
+
+        public static void Associate(this IOrganizationService service, EntityReference reference, Relationship relationship, EntityReferenceCollection relatedEntities)
+        {
+            service.Associate(reference.LogicalName, reference.Id, relationship, relatedEntities);
+        }
+
+        public static void Disassociate(this IOrganizationService service, EntityReference reference, Relationship relationship, EntityReferenceCollection relatedEntities)
+        {
+            service.Disassociate(reference.LogicalName, reference.Id, relationship, relatedEntities);
+        }
+    }
+}
