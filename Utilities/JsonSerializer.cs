d8b0541039e7b6c53257d046f57cc64ecf1a97c2
Derek Finlinson <dcfinlinson@gmail.com>
2021-10-28 17:41:16 -0600

    Add JSON serializer

---
+++ Utilities/JsonSerializer.cs
@@ -0,0 +1,32 @@
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Xrm
+{
+    public static class JsonSerializer
+    {
+        public static string SerializeJson<T>(T instance)
+        {
+            byte[] json;
+
+            using (var stream = new MemoryStream())
+            {
+                var serializer = new DataContractJsonSerializer(typeof(T));
+                serializer.WriteObject(stream, instance);
+                json = stream.ToArray();
+            }
+
+            return Encoding.UTF8.GetString(json);
+        }
+        
+        public static T DeserializeJson<T>(string json)
+        {
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(T));
+                return (T)serializer.ReadObject(stream);
+            }
+        }
+    }
+}
